41230a576d1cf3b4d76b6d94f60fc7556a6c9b30
Akili Humphrey <akilihumphrey@gmail.com>
2019-03-16 08:21:58 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,11 +1,11 @@
 class Ship
-  attr_accessor :name , :type , :booty_attributes
+  attr_accessor :name , :type , :booty
   Ships = []
   
   def initialize(params)
     @name = params[:name]
     @type = params[:type]
-    @booty_attributes = params[:booty_attributes]
+    @booty = params[:booty]
   end
   
    def self.all
